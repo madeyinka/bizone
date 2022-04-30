6c30c36cd2ca8ed1e8298580559d24971b2c0c56
madeyinka007 <mutairadeyinka@yahoo.com>
2022-04-30 10:15:30 +0100

    Home route

--- src/containers/Pages/Landing/Home/index.js
+++ src/containers/Pages/Landing/Home/index.js
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react'
 function HomeComponent() {
 
     useEffect(() => {
-        window.location.href='https://hubit-landing.vercel.app/'
+        window.location.href='https://hubit-main.vercel.app/'
     })
   return (
     <></>
